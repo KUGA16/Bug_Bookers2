23b43e2f0169234eeeebf195078eedb5749cff37
KUGA16 <gachan_1216@yahoo.co.jp>
2020-02-06 07:15:17 +0000

    Change favoriteのルーティングをbooksにネストし追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   resources :users, only: [:show,:index,:edit,:update]
-  resources :books
+  resources :books, only: [:show,:index,:new,:create,:edit,:update,:destroy] do
+    resource :favorites, only: [:create,:destroy]
+  end
 
   root 'home#top'
   get 'home/about'
