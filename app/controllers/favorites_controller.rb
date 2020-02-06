77a7bb1a8eed15a4db765244e63562693d0cb4cc
KUGA16 <gachan_1216@yahoo.co.jp>
2020-02-06 08:22:30 +0000

    Change createアクションとdestroyアクション中身追加、定義

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,8 +1,16 @@
-class FavoritesController < ApplicationController  
+class FavoritesController < ApplicationController
   def create
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorite.new(book.id: book_id)
+    favorite.save
+    redirect_to :back
   end
 
   def destroy
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorite.find_by(book.id: book_id)
+    favorite.destoy
+    redirect_to :back
   end
 
 end
