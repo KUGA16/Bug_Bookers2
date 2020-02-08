944de6841ffd5372e9eeb2742c4c6aabd9be5747
KUGA16 <gachan_1216@yahoo.co.jp>
2020-02-08 09:27:34 +0000

    Change userとrelationshipとのリレーションを定義

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,8 @@
 class Relationship < ApplicationRecord
+  #フォローする側
+  belongs_to :following, class_name: "User"
+  #フォローされる側
+  belongs_to :follower, class_name: "User"
+  # belongs_to(自分でつけたモデル名),class_name(本当のモデル名)
+  validates :following_id, presence: true
 end
