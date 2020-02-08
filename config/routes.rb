3e12b645f44d6a4c0aed929f9b355e20901f4d27
KUGA16 <gachan_1216@yahoo.co.jp>
2020-02-08 08:37:53 +0000

    Change relationshipのルーティングを作成、usersにネスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :users, only: [:show,:index,:edit,:update]
+  resources :users, only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create,:destroy]
+  end
   resources :books, only: [:show,:index,:new,:create,:edit,:update,:destroy] do
     resource :favorites, only: [:create,:destroy]
     resource :book_comments, only: [:create,:destroy]
