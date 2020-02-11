82823b1924a2eba0a50efcec5960ef5d6bd3e377
KUGA16 <gachan_1216@yahoo.co.jp>
2020-02-11 12:52:20 +0000

    Add chatチャンネルを追加

---
+++ app/channels/chat_channel.rb
@@ -0,0 +1,9 @@
+class ChatChannel < ApplicationCable::Channel
+  def subscribed #クライアントと接続されたとき
+    # stream_from "some_channel"
+  end
+
+  def unsubscribed #接続が解除されたとき
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
