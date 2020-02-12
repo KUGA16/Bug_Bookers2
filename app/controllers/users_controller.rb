d3dbe46641d2428fd3b3288e366283a73f43751b
KUGA16 <gachan_1216@yahoo.co.jp>
2020-02-12 09:18:33 +0000

    Change user_findをbeforeアクションにして記述を減らした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,13 @@ class UsersController < ApplicationController
 
 	before_action :authenticate_user!
 	before_action :baria_user, only: [:edit, :update]
+	before_action :user_find, only: [:show,:edit,:update,:follows,:followers]
 
   def show
-  	@user = User.find(params[:id])
+  	# @user = User.find(params[:id])
   	@books = @user.books
   	@book = Book.new #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
+
   end
 
   def index
@@ -15,11 +17,11 @@ class UsersController < ApplicationController
 	end
 
   def edit
-  	@user = User.find(params[:id])
+  	# @user = User.find(params[:id])
   end
 
   def update
-  	@user = User.find(params[:id])
+  	# @user = User.find(params[:id])
   	if @user.update(user_params)
   		redirect_to user_path(@user), notice: "successfully updated user!"
   	else
@@ -28,26 +30,32 @@ class UsersController < ApplicationController
   end
 
 	def follows
-		user = User.find(params[:id])
-		@users = user.followings
+		# @user = User.find(params[:id])
+		@users = @user.followings
 	end
 
 	def followers
-		user = User.find(params[:id])
-		@users = user.followers
+		# @user = User.find(params[:id])
+		@users = @user.followers
 	end
 
+
+
   private
+
   def user_params
   	params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
   #url直接防止　メソッドを自己定義してbefore_actionで発動。
-   def baria_user
+  def baria_user
 			 user = User.find(params[:id])
   	unless user.id == current_user.id
   		redirect_to user_path(current_user)
   	end
-   end
+  end
 
+	def user_find
+		@user = User.find(params[:id])
+	end
 end
