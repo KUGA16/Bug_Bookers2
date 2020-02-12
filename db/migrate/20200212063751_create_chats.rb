d9e39c720c7d4dffcb5bdae8161325e86a8f1a5a
KUGA16 <gachan_1216@yahoo.co.jp>
2020-02-12 08:17:40 +0000

    Add チャットの為のモデル、テーブルを追加

--- db/migrate/20200212063751_create_chats.rb
+++ db/migrate/20200212063751_create_chats.rb
@@ -2,7 +2,8 @@ class CreateChats < ActiveRecord::Migration[5.2]
   def change
     create_table :chats do |t|
       t.text :content
-
+      t.integer :user_id
+      
       t.timestamps
     end
   end
