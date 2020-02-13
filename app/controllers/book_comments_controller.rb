d705621f74609417b10f9c4b85530ba683f8b5f5
KUGA16 <gachan_1216@yahoo.co.jp>
2020-02-13 07:05:33 +0000

    Fix コメント機能非同期修正

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,22 +2,23 @@ class BookCommentsController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
-    @book_comment = current_user.book_comments.new(book_comment_params)
-    @book_comment.book_id = @book.id
+    @book_comment = @book.book_comments.new(book_comment_params)
+    @book_comment.user_id = current_user.id
     if @book_comment.save
-      redirect_back(fallback_location: root_path)
+      flash[:success] = "Comment was successfully created."
+      # redirect_back(fallback_location: root_path)
     else
-      @user = @book.user
-      @new_book = Book.new
+      # @book_comments = BookComment.where(id: @book)
       render "books/show"
     end
   end
 
   def destroy
-    book_comment = BookComment.find(params[:book_id])
-    if book_comment.user_id == current_user.id
-       book_comment.destroy
-       redirect_back(fallback_location: root_path)
+    @book_comment = BookComment.find(params[:book_id])
+    @book = @book_comment.book
+    if @book_comment.user == current_user
+       @book_comment.destroy
+       # redirect_back(fallback_location: root_path)
     else
       redirect_to 'root_path'
     end
@@ -27,5 +28,6 @@ class BookCommentsController < ApplicationController
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
+
 
 end
